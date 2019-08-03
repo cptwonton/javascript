b977584f4187d8f5814a4ce0827cd6fae51bcb5b
Jeff Hong <jeffrey.s.hong@usps.gov>
2019-08-03 07:59:17 -0500

    day 1

--- 2-JS-basics/starter/script.js
+++ 2-JS-basics/starter/script.js
@@ -1,4 +1,4 @@
-console.log('derp')
+/*console.log('derp')
 
 //Multiple assignments
 var x, y;
@@ -10,3 +10,96 @@ x *= 2;
 console.log(x,y);
 x--;
 console.log(x,y);
+*/
+/*************************
+* variables and data types
+*/
+
+/*
+
+console.log('Hello World!!!');
+var firstName = 'John';
+console.log(firstName);
+
+var lastName = 'Smith';
+var age = '28';
+
+var fullAge = true;
+
+console.log(fullAge);
+
+var job = 'Teacher';
+console.log(job);
+
+//single line comment derp herp merp
+
+var years = 3;
+
+*/
+
+
+/*******************************
+ * Variable mutation and type coercion
+ */
+/*
+ var firstName = 'John';
+ var age = 28;
+
+ console.log(firstName + ' ' + age);
+
+ var job, isMarried;
+ job = 'teacher';
+ isMarried = false;
+
+ console.log(firstName + ' is a ' + age + ' year old ' + job + '. Is he married? ' + isMarried);
+
+// Variable mutation
+age = 'twenty eight';
+job = 'driver';
+
+
+//alert(firstName + ' is a ' + age + ' year old ' + job + '. Is he married? ' + isMarried);
+
+//var lastName = prompt('What is his last name?');
+//console.log(lastName);
+
+var now, yearJohn, yearMark;
+now = 2020;
+ageJohn = 28;
+ageMark = 33;
+
+yearJohn = now - 28;
+yearMark = now - 33;
+console.log(yearJohn);
+console.log(now + 2);
+console.log(now/10);
+console.log(now * 2);
+
+// logical operators
+var johnOlder = ageJohn < ageMark;
+console.log(johnOlder);
+
+// typeof operator
+console.log(typeof johnOlder);
+console.log(typeof ageJohn);
+console.log(typeof ' ');
+console.log(typeof undefined);
+console.log(typeof x);
+*/
+
+
+/***********************************
+ * Operator precedence
+ */
+
+var now = 2018;
+var yearJohn = 1989;
+var fullAge = 18;
+
+var isFullAge = now - yearJohn >= fullAge;
+console.log(isFullAge);
+
+var ageJohn = now - yearJohn;
+var ageMark = 35;
+var average = (ageJohn + ageMark) / 2
+console.log(average);
