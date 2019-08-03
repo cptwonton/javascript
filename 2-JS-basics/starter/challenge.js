bcc9a23f0a3d480f8f3fff0e79c6fa36781d4d9f
Jeff Hong <jeffrey.s.hong@usps.gov>
2019-08-03 09:01:48 -0500

    halfway done with js basics y33t

--- 2-JS-basics/starter/challenge.js
+++ 2-JS-basics/starter/challenge.js
@@ -87,6 +87,7 @@ if (markBMI > johnBMI) {
  * Boolean logic
  */
 
+ /*
  var firstName = 'John';
  var age = 16;
 
@@ -94,8 +95,105 @@ if (markBMI > johnBMI) {
      console.log(firstName + ' is a boy.');
  } else if (age < 20) { // between 13 and 20
     console.log(firstName + ' is a teenager.');
+ } else if (age < 30) {
+    console.log(firstName + ' is a young man.');
  }
  else {
      console.log(firstName + ' is a man.');
  }
+
+*/
+
+/**
+ * The Ternary Operator and Switch Statements
+ */
+
+ /*
+ var firstName = 'John';
+ var age = 16;
+
+ console.log(firstName + ((age >= 18) ? ' would like a beer.' : ' would like a juice box.'));
+
+var job = 'driver';
+
+switch (job) {
+    case 'teacher':
+        console.log(firstName + ' teaches kids how to code.');
+        break;
+    case 'driver':
+        console.log(firstName + ' drives an uber in Lisbon.');
+        break;
+    case 'designer':
+        console.log(firstName + ' designs beautiful websites.');
+        break;
+    default:
+        console.log(firstName + ' does something else.');
+}
+
+switch (true) {
+    case age < 13:
+        console.log(firstName + ' is a boy.');
+        break;
+    case age < 20:
+        console.log(firstName + ' is a teenager.');
+        break;
+    case age < 30:
+        console.log(firstname + ' is a young man.');
+        break;
+    default:
+        console.log(firstName + ' is a man.');
+}
+
+*/
+
+/**
+ * Truthy and Falsy values and equality operators
+ */
+
+ // falsy values: undefined, null, 0, '', NaN
+ // truthy values: NOT falsy values
+/*
+ var height;
+
+ height = 23;
+
+ if (height || height === 0) {
+     console.log('Variable is defined.');
+ } else {
+     console.log('Variable has NOT been defined.');
+ }
+
+// Equality operators
+if (height == '23') {
+    console.log('The == does type coercion!');
+}
+*/
+/*
+var johnAverage = (89+120+103)/3;
+var mikeAverage = (116+94+123)/3;
+console.log(johnAverage, mikeAverage);
+
+if (johnAverage === mikeAverage) {
+    console.log('it\'s a tie! Score is : johnAverage');
+} else if (johnAverage > mikeAverage) {
+    console.log('John\'s team has won! Final average: ' + johnAverage);
+} else {
+    console.log('Mike\'s team has won! Final average: ' + mikeAverage);
+}
+*/
+
+/**
+ * Functions
+ */
+
+
+
+
+
+
+
+
+
+
+
 
